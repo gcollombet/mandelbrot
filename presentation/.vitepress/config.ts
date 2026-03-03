d48a54ea89a5a6fda66c2903b7e013ebd375b2a1
Collombet Guillaume <guillaume.collombet@orange.com>
2026-03-03 19:41:31 +0100

    feat: iter

--- presentation/.vitepress/config.ts
+++ presentation/.vitepress/config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
         nav: [
             {text: 'Home', link: '/'},
             {text: 'Optimisations', link: '/optimisation'},
-            {text: 'Explorer', link: '/mandelbrot/'},
         ],
         sidebar: [
             {
--- presentation/.vitepress/theme/index.ts
+++ presentation/.vitepress/theme/index.ts
@@ -1,11 +1,13 @@
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import ImageViewerP from '@miletorix/vitepress-image-viewer'
 import '@miletorix/vitepress-image-viewer/style.css'
+import CustomLayout from './CustomLayout.vue'
 
 import './custom.css'
 
 export default {
     extends: DefaultTheme,
+    Layout: CustomLayout,
     enhanceApp(ctx) {
         ImageViewerP(ctx.app)
     }
